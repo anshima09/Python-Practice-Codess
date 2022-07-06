0c6a513b33d5a074c3d2feb912ed0be29d63992c
Anshima Sharma <84280918+anshima09@users.noreply.github.com>
2022-07-05 23:39:10 -0700

    Add files via upload

---
+++ emoji.py
@@ -0,0 +1,31 @@
+print("\U0001F600")
+print("\U0001F923")
+print("\U0001F609")
+print("\U0001F603")
+print("\U0001F604")
+print("\U0001F601")
+print("\U0001F606")
+print("\U0001F605")
+print("\U0001F923")
+print("\U0001F602")
+print("\U0001F642")
+print("\U0001F643	")
+print("\U0001F609	")
+
+print("\U0001F60A	")
+
+print("\U0001F607	")
+
+print("\U0001F970	")
+
+print("\U0001F60D")
+
+print("\U0001F929")
+
+print("\U0001F618")
+
+print("\U0001F617	")
+print("\U0001F61A")
+
+
+
